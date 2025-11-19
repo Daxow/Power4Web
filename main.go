ebaec8f6eeb2fb5289f5734fd41cd65aab5526d2
Daxow <voxizofficiel@gmail.com>
2025-11-19 10:16:42 +0100

    add game.go

--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-type Game struct {
-	Board  [6][7]int
-	Player int
-	Winner int
-}
-
-var game = Game{
-	Player: 1,
-}
-
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Puissance 4")
-	fmt.Fprintln(w, "Joueur:", game.Player, "Gagnant:", game.Winner)
-	for i := 0; i < 6; i++ {
-		for j := 0; j < 7; j++ {
-			fmt.Fprint(w, game.Board[i][j], " ")
-		}
-		fmt.Fprintln(w)
-	}
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("test")
 }
